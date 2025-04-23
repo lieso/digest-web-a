4f027442e76ff9001f5093582422934b624fddbc
David Roussov <david.roussov@gmail.com>
2025-04-23 17:27:10 +1000

    render simple list of titles

--- src/components/digest/index.tsx
+++ src/components/digest/index.tsx
@@ -1,16 +1,35 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Entry = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Title = styled.a`
+  font-size: 24px;
+`;
 
 interface DigestProps {
   digest: any;
 }
 
-export const Digest: React.FC<DigestProps> = ({ digest }) => {
+export const Digest: React.FC<DigestProps> = ({ digest }: any) => {
   return (
-    <div style={{ color: 'blue', fontSize: '20px' }}>
-      digest works!
-
-      {JSON.stringify(digest)}
-    </div>
+    <Container>
+      { digest.entries.map(digest => (
+        <Entry>
+          <Title>
+            {digest.link_title}
+          </Title>
+        </Entry>
+      ))}
+    </Container>
   );
 };
 
